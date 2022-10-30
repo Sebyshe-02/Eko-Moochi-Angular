cf06fa9a5b6d07f003c2a21831f4df1e644eaf7b
Sebastian <sebastian.henriquez.c@mail.pucv.cl>
2022-10-30 00:01:28 -0300

    Actualizacion del home del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { CarruselComponent } from './carrusel/carrusel.component';
+import { ComponentModule } from './component/component.module';
+import { PagesModule } from './pages/pages.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    FooterComponent,
-    CarruselComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ComponentModule,
+    PagesModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
