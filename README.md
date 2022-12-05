4f8a3916ad278f2e01eaf21e41829ac3371b593c
Sebastian <sebastian.henriquez.c@mail.pucv.cl>
2022-12-04 21:42:13 -0300

    Actualizacion Final

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # EkoMoochi
 
+Integrantes: Luz Pinto Castillo, Sebastian Henriquez Cartagena
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.6.
 
 ## Development server
--- src/app/component/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/component/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl, FormBuilder} from '@angular/forms';
+
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./iniciar-sesion.component.scss']
 })
 export class IniciarSesionComponent implements OnInit {
+  formularioLogin!: FormGroup;
 
-  constructor() { }
+  constructor(private readonly loginForm: FormBuilder) {}
 
   ngOnInit(): void {
+    this.formularioLogin = this.initForm();
+  };
+
+  initForm(): FormGroup {
+    return this.loginForm.group({
+      email: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required]]
+    })
   }
 
+  onSubmit(): void {
+    console.log('Form ->');
+  }
 }
